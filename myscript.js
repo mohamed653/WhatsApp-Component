72c2c47eeb6d8f458e551a1ded3608504a7804e5
Mohamed Hamed <Mohamedhamed3343@gmail.com>
2024-05-01 21:05:01 +0300

    changing number

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
 $(function () {
   $("#WAButton").floatingWhatsApp({
-    phone: "201556551942", //WhatsApp Business phone number International format-
+    phone: "0000000", //WhatsApp Business phone number International format-
     //Get it with Toky at https://toky.co/en/features/whatsapp.
     headerTitle: "Message", //Popup Title
     popupMessage: "Hello, How Can We Help You", //Popup Message
