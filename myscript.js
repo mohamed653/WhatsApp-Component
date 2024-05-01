8aca9f3547370eeb8315953e82e0c1379ec95de6
Mohamed Hamed <Mohamedhamed3343@gmail.com>
2024-05-01 20:44:27 +0300

    whatsappTest

---
+++ myscript.js
@@ -0,0 +1,13 @@
+$(function () {
+  $("#WAButton").floatingWhatsApp({
+    phone: "201556551942", //WhatsApp Business phone number International format-
+    //Get it with Toky at https://toky.co/en/features/whatsapp.
+    headerTitle: "Message", //Popup Title
+    popupMessage: "Hello, How Can We Help You", //Popup Message
+    showPopup: true, //Enables popup display
+    buttonImage: '<img src="./whatsapp/whatsapp.svg"/>', //Button Image
+    headerColor: "green", //Custom header color
+    //backgroundColor: 'crimson', //Custom background button color
+    position: "right",
+  });
+});
